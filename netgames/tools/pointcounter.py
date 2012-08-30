8673dfb33a9d328bb077ff2e83f1111205e01474
therauli <therauli@host-001.elisa>
2012-08-30 11:46:48 +0300

    CP is now calculated. Using wrong value for N_C

--- netgames/tools/pointcounter.py
+++ netgames/tools/pointcounter.py
@@ -1,12 +1,19 @@
-import operator
 import read_asdoc_xml as r
 #import read_jsdoc_json as r
 
-def prod(operands):
-	return reduce(operator.mul, operands)
+def getN(c, C):
+	term = len(c.fields) + len(c.relations) + len(c.methods)
+	result = 1
+	if c.supers:
+		return 0.5
+	return 1
 
+	#Fix this if time
+	for x in c.supers:
+		result *= term / (term + len(C[x].fields) + len(C[x].relations) + len(C[x].methods))
+	return result
 
-C = r.giev().values()
+C = r.giev()
 
 W_c = 4
 W_R_c = 2
@@ -15,20 +22,15 @@ W_O_M = 2
 W_S_O = 2
 W_T_O = 2
 
-N_C = sum([c.N_c for c in C]) / len(C)
-CP = (W_c * len(C) + sum([len(c.fields) for c in C]) + W_R_c * sum([len(c.relations) for x in C]) + W_O_c * sum([len(c.methods) for x in C])) * N_C
+
+N_C = sum([getN(c, C) for c in C.values()]) / len(C)
+
+
+CP = (W_c * len(C) + sum([len(c.fields) for c in C.values()]) + W_R_c * sum([len(c.relations) for x in C.values()]) + W_O_c * sum([len(c.methods) for x in C.values()])) * N_C
 print CP
 
-
-
-# N_O_M = sum([o.N for o in O_M]) / len(O_M)
-
-
-
-
+#N_O_M = sum([o.N for o in O_M]) / len(O_M)
 
 # MP = (W_O_M * len(O_M) + sum([len(o.P) for o in O_M)]) + W_S_O * sum([len(o.S) for i in O_M]) + W_T_O * sum([len(o.T) for o in O_M])) * N_O_M
-
-
 
 # N_c = prod([(len(c.A) + len(c.R) + len(c.O)) / (len(c.A) + len(c.R) + len(c.O) + ) for c in C])
