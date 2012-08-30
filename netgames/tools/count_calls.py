dbd571fe2fbd68ab09ddac18b74e9add4cb045ee
antont <toni@playsign>
2012-08-30 16:18:29 +0300

    make Erno's call tree parser a module that can be called for all the classes

--- netgames/tools/count_calls.py
+++ netgames/tools/count_calls.py
@@ -1,40 +1,50 @@
+DATAPATH = "../data/unionpong/"
+FILESUFFIX = "-filtered.tree"
+
 def do_call_block(last_call, exprs):
     first_tokens = [x[0] for x in exprs[1:]]
     print last_call, 'had', first_tokens.count('NAME'),
-    print 'names and', first_tokens.count('NUMBER'), 'numbers'
+    print 'names and', first_tokens.count('NUMBER'), 'numbers',
+    print 'and', first_tokens.count('STRING'), 'strings, total: %d' % len(first_tokens)
 
-prev_il = 0
-in_call = False
-curr = []
-last_call = None
-for line in open("unionpong_court_closuretree-callsfiltered.txt"):
-    indent_spaces = len(line) - len(line.lstrip(' '))
-    if indent_spaces % 4 or '\t' in line:
-        sys.exit('bad indent, need spaces in multiples of 4')
-    indent_level = indent_spaces / 4
+def count_calls(classname):
+    calltreefile = "%s%s%s" % (DATAPATH, classname, FILESUFFIX)
 
-    tokens = line.strip().split()
+    prev_il = 0
+    in_call = False
+    curr = []
+    last_call = None
+    for line in open(calltreefile):
+        indent_spaces = len(line) - len(line.lstrip(' '))
+        if indent_spaces % 4 or '\t' in line:
+            sys.exit('bad indent, need spaces in multiples of 4')
+        indent_level = indent_spaces / 4
 
-    if indent_level > prev_il:
-        curr = []
+        tokens = line.strip().split()
 
-    if indent_level < prev_il:
-        if in_call:
-            do_call_block(last_call, curr)
+        if indent_level > prev_il:
+            curr = []
 
-        in_call = False
-        curr = []
+        if indent_level < prev_il:
+            if in_call:
+                do_call_block(last_call, curr)
 
-    if tokens[0] == 'CALL':
-        in_call = True
-        last_call = tokens
+            in_call = False
+            curr = []
 
-
-    curr.append(tokens)
-
-    prev = curr
-    prev_il = indent_level
+        if tokens[0] == 'CALL':
+            in_call = True
+            last_call = tokens
 
 
-if in_call:
-    do_call_block(last_call, curr)
+        curr.append(tokens)
+
+        prev = curr
+        prev_il = indent_level
+
+
+    if in_call:
+        do_call_block(last_call, curr)
+
+if __name__ == '__main__':
+    count_calls("Court")
