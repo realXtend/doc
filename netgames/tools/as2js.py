c67d45c68e7733fdb36bccd2832c6c2c5cca07aa
therauli <therauli@host-001.elisa>
2012-08-30 14:37:30 +0300

    removed backupfiles

--- netgames/tools/as2js.py
+++ netgames/tools/as2js.py
@@ -4,14 +4,15 @@ import os
 path = '../UnionPong/Actionscript/'
 
 type_matcher = re.compile(r'(:\w+)')
-visibilities = re.compile(r'public|static|protected|private')
+keywords = re.compile(r'public|static|protected|private|final')
 package = re.compile(r'(^package \{)(.*)}')
 
 for filename in os.listdir(path):
     data = open(os.path.join(path, filename)).read()
     #print type_matcher.findall(data)
     data = type_matcher.sub('', data)
-    data = visibilities.sub('', data)
+    data = keywords.sub('', data)
+    data = data.replace('const ', 'var ')
     print data
     open(os.path.join('../UnionPong/js_for_closure/', filename.replace('.as', '.js')), 'w').write(data)
 
