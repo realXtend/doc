d7ad5781163a9f0d6d76620e8a4fb653f72a685d
antont <toni@playsign>
2012-09-13 02:15:15 +0300

    apply IEEE formatting to the PDF, via latex. little tweaks to rst and the rst2latex command with the suitable options in makelatex.sh

---
+++ netgames/makelatex.sh
@@ -0,0 +1,1 @@
+rst2latex.py --use-latex-citations --documentclass=IEEEtran --documentoptions=conference --use-latex-abstract api_complexity_analysis.rst --no-section-numbering > api_complexity_analysis.tex
